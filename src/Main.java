1fdc0802653fefb77a265e788465bfba27135bf4
leila <leila.m2006131@gmail.com>
2025-03-08 12:54:28 +0330

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import payment.CreditCardPayment;
+import payment.PayPalPayment;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/payment/Customer.java
@@ -0,0 +1,4 @@
+package payment;
+
+public abstract class Customer {
+}
